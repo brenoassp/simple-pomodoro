09cdc6129b9480d2cd189eaa6642ab3aa69b8c78
breno machado <breno.asrm@gmail.com>
2022-11-04 13:44:20 -0300

    5min rest

--- pomodoro.sh
+++ pomodoro.sh
@@ -4,5 +4,8 @@ POMODORO_EM_MINUTOS=$1
 while [ 1 ]
 do
 	sleep $(($POMODORO_EM_MINUTOS*60))
-	feh pomodoro.webp
+	feh pomodoro.webp &
+	img_pid=$!
+	sleep 300 #300 segundos = 5min repouso
+	kill $img_pid
 done
